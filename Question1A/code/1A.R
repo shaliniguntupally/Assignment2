f261f14498495ae1fb78783a518a15efd553b5b2
shaliniguntupally <146143130+shaliniguntupally@users.noreply.github.com>
2023-11-08 05:53:13 +0530

    Add files via upload

--- Question1A/code/1A.R
+++ Question1A/code/1A.R
@@ -5,9 +5,14 @@ data <- read.csv("D:/train.csv")
 missing_values <- colSums(is.na(data))
 
 # Display columns with missing values
-print(missing_values)
+print(missing_values[missing_values > 0])
 
 # Impute missing values in "seats" with the mode (most common value)
 mode_val <- as.numeric(names(sort(table(data$seats), decreasing = TRUE)[1]))
 data$seats[is.na(data$seats)] <- mode_val
 print(data)
+
+# 'New_Price' is the column name to be imputed
+median_new_price <- median(data$New_Price, na.rm = TRUE)
+data$New_Price[is.na(data$New_Price)] <- median_new_price
+head(data)
